7f4ec251d7df8cf249252e8602c442574a92a0dc
Ronny Valdivieso <ronnyvaldivieso25@gmail.com>
2021-10-25 02:23:09 -0500

    Color de respuesta incorrecta. Corrección de flujo

--- src/components/options.js
+++ src/components/options.js
@@ -4,10 +4,16 @@ function Options(props) {
 	const questionId = props.questionId;
 	const	options = props.options;
 	const [customClass, setClass] = useState('');
-	const handleClick = (op) => {
+	const handleClick = (op, index) => {
 		if (questionId === op.nextOption) {
 			op.text = op.answer;
-			setClass('option-answer');
+			// setClass('option-answer');
+			var elements = document.querySelectorAll('.option');
+			console.log(elements);
+			var currentElement = Array.from(elements)
+				.filter(x => x.getAttribute('data-key') == index)[0];
+			console.log(currentElement);
+			currentElement.className = 'option-answer';
 		}
 		props.onSelectedOption(op.nextOption);
 	}
@@ -16,9 +22,9 @@ function Options(props) {
 		const items = options.map(op => {
 			const index = options.indexOf(op);
 			return (
-				<li key={index}>
+				<li className='option' key={index} data-key={index}>
 					{questionId !== 5 
-					? <button onClick={() => handleClick(op)}>{op.text}</button> 
+					? <button onClick={() => handleClick(op, index)}>{op.text}</button> 
 					: <a href={op.answer} target="_blank" rel="noreferrer">{op.text}</a>}
 				</li>
 			);
